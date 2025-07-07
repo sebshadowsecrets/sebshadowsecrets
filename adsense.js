8c13c4971dc2e43dbddc4c26bdcfb699b80bdad7
sebshadowsecrets <sebmindblownfacts@gmail.com>
2025-07-07 21:44:22 +0200

    Update adsense.js

--- adsense.js
+++ adsense.js
@@ -1,1 +1,7 @@
-(adsbygoogle = window.adsbygoogle || []).push({});
+(function() {
+  var ads = document.createElement('script');
+  ads.async = true;
+  ads.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+  ads.setAttribute('data-ad-client', "ca-pub-5000547699906372");
+  document.head.appendChild(ads);
+})();
